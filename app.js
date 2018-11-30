068e63b353eb356822e02c4d442cf04c14a7d42b
Rodrigo Gutierrez <rodrigo@mawari.co.jp>
2018-11-30 15:46:00 +0900

    add sorting of campaign data, move 'json2table' function

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const server = http.createServer((req, res) => {
 
 	var result = input.replace("\"{{chartData}}\"", JSON.stringify(chartData));
 	
-	var addTable = result.replace("{{table}}", json2table(data.Items));
+	var addTable = result.replace("{{table}}", json2table(campaigns));
 
 	res.write(addTable);
 	res.end();
@@ -34,6 +34,54 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
 	console.log("Mawari Data Visualizer Node.js Server now running and listening on port " + port);
 });
+
+// configure the request
+const options = {
+	url: "https://hgy62pa5ib.execute-api.ap-northeast-1.amazonaws.com/default/mawariReportGetter",
+	method: "GET",
+	headers: {
+		"Content-Type": "application/json",
+		"x-api-key": "AL7NxngP8h19POfGnDXto9oGmgdXE2kz2yTnzhtl"
+		// need to read this from un-committed file later
+	}
+};
+
+// request loop
+(function requestLoop() {
+	request(options, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+	        data = JSON.parse(body);
+	        console.log("ScannedCount: " + data.ScannedCount);
+	        sortCampaigns();
+	    }
+	    else {
+	    	console.log(response.statusCode + ": " + error);
+	    	data = null;
+	    }
+	});
+	setTimeout(requestLoop, 600000);
+})();
+
+function Campaign(creativeId) {
+	this.creativeId = creativeId;
+	this.impressionCount = 0;
+	this.interactionCount = 0;
+	this.clickCount = 0;
+};
+
+function sortCampaigns() {
+	data.Items.forEach(item => {
+		var campaign = campaigns.find(c => c.creativeId == item.creativeId);
+        if (campaign == null) {
+            campaigns.push(new Campaign(item.creativeId));
+            campaign = campaigns[campaigns.length - 1];
+        }
+
+        campaign.impressionCount += item.impressionCount;
+        campaign.interactionCount += item.interactionCount;
+        campaign.clickCount += item.clickCount;
+	});	
+};
 
 function json2table(json) {
 	var cols = Object.keys(json[0]);
@@ -53,31 +101,4 @@ function json2table(json) {
 	});
 
 	return "<table><thead><tr>" + headerRow + "</tr></thead><tbody>" + bodyRows + "</tbody></table>"; 
-}
-
-var data;
-
-// configure the request
-const options = {
-	url: "https://hgy62pa5ib.execute-api.ap-northeast-1.amazonaws.com/default/mawariReportGetter",
-	method: "GET",
-	headers: {
-		"Content-Type": "application/json",
-		"x-api-key": "AL7NxngP8h19POfGnDXto9oGmgdXE2kz2yTnzhtl"
-	}
 };
-
-// request loop
-(function requestLoop() {
-	request(options, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-	        data = JSON.parse(body);
-	        console.log("ScannedCount: " + data.ScannedCount);
-	    }
-	    else {
-	    	console.log(response.statusCode + ": " + error);
-	    	data = null;
-	    }
-	});
-	setTimeout(requestLoop, 600000);
-})();
